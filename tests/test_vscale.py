bb167c0f2b697d8bc78fae6652fc22d83c64682f
Siarhei Karol <m.jjesire@gmail.com>
2021-03-21 00:24:21 +0300

    add:test2/test3

--- tests/test_vscale.py
+++ tests/test_vscale.py
@@ -17,10 +17,24 @@ def test_list_key_pairs():
 
 
 @vcr.use_cassette("./tests/fixtures/get_key_pair.yaml", filter_headers=["X-Token"])
-def test_get_key_pair():
+def test1_get_key_pair():
     conn = VscaleDriver(key=os.getenv("VSCALE_TOKEN"))
     key = conn.get_key_pair("x200s")
     assert key.name == "x200s"
     assert key.fingerprint == "ca:28:dc:6e:9e:72:48:d3:bc:73:76:19:d2:5f:c3:e8"
     assert isinstance(key.driver, VscaleDriver)
     assert key.extra["id"] == 46329
+
+
+@vcr.use_cassette("./tests/fixtures/get_key_pair.yaml", filter_headers=["X-Token"])
+def test2_get_key_pair():
+    conn = VscaleDriver(key=os.getenv("VSCALE_TOKEN"))
+    key = conn.get_key_pair("x200s")
+    assert key.name != "MissingName"
+
+
+@vcr.use_cassette("./tests/fixtures/get_key_pair_empty.yaml", filter_headers=["X-Token"])
+def test3_get_key_pair():
+    conn = VscaleDriver(key=os.getenv("VSCALE_TOKEN"))
+    key = conn.get_key_pair("x200s")
+    assert key is None
